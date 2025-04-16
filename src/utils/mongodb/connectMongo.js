f155a7dfab49ddd2a721c6de01428b88049e9c2a
AlmondThree <donnyfadhillah23@gmail.com>
2025-04-16 19:46:23 +0700

    DEV: APS-34 Singleton code does not work

--- src/utils/mongodb/connectMongo.js
+++ src/utils/mongodb/connectMongo.js
@@ -3,12 +3,12 @@ const mongoUri = require("../../config/js/mongoUri");
 
 const uri = mongoUri.connectDB("traxify_master");
 
+var instance;
+
 const connectMongo = async () => {
     function newConnection() {
         return new MongoClient(uri);
     }
-
-    var instance;
 
     return {
         getInstance : function () {
