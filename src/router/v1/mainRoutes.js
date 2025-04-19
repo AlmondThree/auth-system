b20254d0430cfa33d31dcb3143894d679a06f533
AlmondThree <donnyfadhillah23@gmail.com>
2025-04-19 16:09:28 +0700

    DEV: APS-37 Create service Refresh Token

--- src/router/v1/mainRoutes.js
+++ src/router/v1/mainRoutes.js
@@ -6,15 +6,16 @@ const Services = require("../../models/Services");
 //Register service
 const { serviceRegister } = require('../../services/register/serviceRegister')
 const { loginUser } = require('../../services/login/serviceLogin')
-const { sericeAuthorizeToken } = require('../../services/authorize/serviceAuthorizeToken');
+const { serviceAuthorizeToken } = require('../../services/authorize/serviceAuthorizeToken');
 const { getListRoles } = require('../../services/roles/serviceGetListRoles');
+const { serviceRefreshToken } = require('../../services/authorize/serviceRefreshToken');
 
 router.route('/*').all(async (req, res, next) => {
     const serviceObj = new Services()
     res.locals = serviceObj.createNextAttribute(404, {
         status: "error",
-        message: "not found",
-    }, null, "not found")
+        message: "resource not found",
+    }, null, res.locals.status)
 
     next();
 })
@@ -58,7 +59,7 @@ router.route('/login/users').all(async (req, res, next) => {
 router.route('/authorize/token').all(async (req, res, next) => {
     switch (req.method) {
         case "POST":
-            await sericeAuthorizeToken(req, res)
+            await serviceAuthorizeToken(req, res)
             next()       
             break;
     
@@ -77,6 +78,24 @@ router.route('/roles').all(async (req, res, next) => {
     switch (req.method) {
         case "GET":
             await getListRoles(req, res)
+            next()       
+            break;
+    
+        default:
+            const serviceObj = new Services()
+            res.locals = serviceObj.createNextAttribute(405, {
+                status: "error",
+                message: "Invalid http methods!",
+            }, null, "Invalid http methods")
+            next()
+            break;
+    }
+})
+
+router.route('/refresh/token').all(async (req, res, next) => {
+    switch (req.method) {
+        case "POST":
+            await serviceRefreshToken(req, res)
             next()       
             break;
     
